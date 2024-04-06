f73bb0209f8e3dfcf784b31decf283aaf6cc1007
tiszczesz <tiszczesz@users.noreply.github.com>
2024-04-06 17:12:57 +0200

    jj

--- server-side/cw3-api/Program.cs
+++ server-side/cw3-api/Program.cs
@@ -1,7 +1,10 @@
+using cw3_api;
+
 var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("SqliteString");
+List<Book> books = BooksRepo.GetBooks(connString);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => books);
 
 app.Run();
