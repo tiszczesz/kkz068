b69c1cae39427e9d7c268d9505372f739fcc5bf0
tiszczesz <tiszczesz@users.noreply.github.com>
2024-04-06 17:09:11 +0200

    jj

---
+++ server-side/cw3-api/Models/BooksRepo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace cw3_api;
+
+public class BooksRepo
+{
+    public static List<Book> GetBooks(string connString)
+    {
+        var books = new List<Book>();
+        using (SqliteConnection conn = new SqliteConnection(connString))
+        {
+            SqliteCommand command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM Books";
+            conn.Open();
+            SqliteDataReader rd = command.ExecuteReader();
+            while (rd.Read())
+            {
+                books.Add(
+                    new Book
+                    {
+                        Id = rd.GetInt32(0),
+                        Title = rd.GetString(1),
+                        Author = rd.GetString(2),
+                        Price = rd.GetDecimal(3)
+                    }
+                );
+            }
+        }
+        return books;
+    }
+}
--- server-side/cw3-api/Program.cs
+++ server-side/cw3-api/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+var connString = builder.Configuration.GetConnectionString("SqliteString");
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
