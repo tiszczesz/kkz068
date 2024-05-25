664c8fdb8fd987b56130a8767be015aed5c61d5f
tiszczesz <tiszczesz@users.noreply.github.com>
2024-05-25 14:03:15 +0200

    jj

---
+++ client-side/cw7-vite-ts/src/data/cars.ts
@@ -0,0 +1,21 @@
+export type Car = {
+    marka: string;
+    przebieg: number;
+    rokProdukcji: number;
+    cena: number;
+}
+
+export const cars: Car[] = [
+    { marka: "Opel", przebieg: 95000, rokProdukcji: 2017, cena: 22000 },
+    { marka: "Renault", przebieg: 80000, rokProdukcji: 2018, cena: 18000 },
+    { marka: "BMW", przebieg: 60000, rokProdukcji: 2020, cena: 45000 },
+    { marka: "Audi", przebieg: 70000, rokProdukcji: 2019, cena: 38000 },
+    { marka: "Mercedes", przebieg: 75000, rokProdukcji: 2018, cena: 42000 },
+    { marka: "Ford", przebieg: 85000, rokProdukcji: 2017, cena: 20000 },
+    { marka: "Volkswagen", przebieg: 90000, rokProdukcji: 2016, cena: 25000 },
+    { marka: "Toyota", przebieg: 100000, rokProdukcji: 2015, cena: 23000 },
+    { marka: "Peugeot", przebieg: 72000, rokProdukcji: 2021, cena: 28000 },
+    { marka: "Hyundai", przebieg: 55000, rokProdukcji: 2022, cena: 32000 },
+];
+
+
