d4164db66e754788da318c1126a9549fd2055c6c
pszczesz <pszczesz@gmail.com>
2024-06-08 08:53:59 +0200

    jj

---
+++ client-side/react-4/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import './App.css'
+
+function App() {  
+
+  return (
+    <div className='container'>
+      <h1>React - 4 Listy</h1>
+    </div>
+  )
+}
+
+export default App
