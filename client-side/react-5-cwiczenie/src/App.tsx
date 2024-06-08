5835bfa09573d68e7c24fe2f0feeb31f2a3ca2ab
pszczesz <pszczesz@gmail.com>
2024-06-08 12:34:52 +0200

    kk

--- client-side/react-5-cwiczenie/src/App.tsx
+++ client-side/react-5-cwiczenie/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [names, setNames] = useState<string[]>(['Anna', 'Monika', 'Roman']);
+  const [firstname, setFirstname] = useState<string>('');
+  console.log(firstname);
+  
+  function handleDelete(index: number): void {
+    setNames(names.filter((name, i) => i!== index));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mt-3">
+      <div className="row">
+        <input
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+          type="text"
+          className="col-3"
+          placeholder="podaj imię zanajomego"
+        />
+        <input type="button" className="col-3" value="Dodaj nowego" 
+        onClick={()=>setNames([...names,firstname])}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h1>Lista zanajomych</h1>
+      <ul className="list-group">
+        {names.map((name, index) => (
+          <li className="list-group-item" key={index}>
+            {name} <button onClick={()=>handleDelete(index)}>x</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
