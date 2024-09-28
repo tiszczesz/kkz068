08fa4e954bed2cd07d3ed9031874762305db9916
pszczesz <tiszczesz@users.noreply.github.com>
2024-09-28 08:51:14 +0200

    hh

--- client-side/2024-25/cw2/src/App.tsx
+++ client-side/2024-25/cw2/src/App.tsx
@@ -1,21 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './App.css'
+import './App.css';
 import ColorComp from './components/ColorComp';
 import Color2Comp from './components/Color2Comp';
 
-
 function App() {
-  const myColors = ['red', 'green', 'blue', 'yellow','pink','#1245ae'];
+  const myColors = ['red', 'green', 'blue', 'yellow', 'pink', '#1245ae'];
 
   return (
-    <div className='container'>
-     <h1>Zrobiłem sam</h1>
-    <ColorComp/>
-    <Color2Comp colors={myColors}>ala ma kota</Color2Comp>
-    <Color2Comp colors={["black","#675554","grey"]}/>
+    <div className="container">
+      <h1>Zrobiłem sam</h1>
+      <ColorComp />
+      <Color2Comp colors={myColors}>ala ma kota</Color2Comp>
+      <Color2Comp colors={myColors}>
+        <h4>Inne dziecko</h4>
+      </Color2Comp>
+      <Color2Comp colors={['black', '#675554', 'grey']} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client-side/2024-25/cw2/src/components/Color2Comp.tsx
+++ client-side/2024-25/cw2/src/components/Color2Comp.tsx
@@ -1,25 +1,36 @@
-import { useState } from "react";
+import { ReactNode, useState } from 'react';
 
 type Props = {
-    colors: string[],
-    text?:string
-}
+  colors: string[];
+  text?: string;
+  children?: ReactNode;
+};
 
 const Color2Comp = (props: Props) => {
   const [color, setColor] = useState(props.colors[0]);
   return (
     <>
-    <select value={color} onChange={(e)=>setColor(e.target.value)}>
-      {props.colors.map((color,index)=>(
-        <option key={index} value={color}>{color}</option>
-      ))}
-    </select>
-    <div
-        style={{ minWidth: '100px', minHeight: '100px', textAlign: 'center',
-            border: 'solid black 1px', backgroundColor: color }}
-      >{props.text}</div>
+      <select value={color} onChange={(e) => setColor(e.target.value)}>
+        {props.colors.map((color, index) => (
+          <option key={index} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
+      <div
+        style={{
+          minWidth: '100px',
+          minHeight: '100px',
+          textAlign: 'center',
+          border: 'solid black 1px',
+          backgroundColor: color,
+        }}
+      >
+        {props.text}
+      </div>
+      {props.children && <div style={{ color: 'green' }}>{props.children}</div>}
     </>
-  )
-}
+  );
+};
 
-export default Color2Comp
+export default Color2Comp;
