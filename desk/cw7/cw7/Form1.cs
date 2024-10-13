8ff395a10e77add3acbb8e148790b7761b5619fd
tiszczesz <tiszczesz@users.noreply.github.com>
2024-10-13 11:47:54 +0200

    jj

---
+++ desk/cw7/cw7/Form1.cs
@@ -0,0 +1,11 @@
+namespace cw7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // string path = AppDomain.CurrentDomain.BaseDirectory;
+        }
+    }
+}
