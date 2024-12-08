6adc0e5e960bcc629974ef1965f6a83aa1470a44
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-08 09:48:57 +0100

    kk

--- desk/ark4/solution/SongsApp/Form1.Designer.cs
+++ desk/ark4/solution/SongsApp/Form1.Designer.cs
@@ -28,33 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            lbArtist = new Label();
+            lbTitle = new Label();
+            lbSongsNumber = new Label();
+            lbdownloadsNumber = new Label();
+            lbYear = new Label();
+            btnDownload = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // pictureBox1
             // 
-            button1.Location = new Point(257, 80);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, 175);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 70);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1289, 175);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 70);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(128, 84);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(199, 201);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // lbArtist
+            // 
+            lbArtist.AutoSize = true;
+            lbArtist.Font = new Font("Segoe UI", 50.25F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            lbArtist.ForeColor = Color.White;
+            lbArtist.Location = new Point(400, 48);
+            lbArtist.Name = "lbArtist";
+            lbArtist.Size = new Size(194, 89);
+            lbArtist.TabIndex = 3;
+            lbArtist.Text = "Artist";
+            // 
+            // lbTitle
+            // 
+            lbTitle.AutoSize = true;
+            lbTitle.Font = new Font("Segoe UI", 30F, FontStyle.Italic, GraphicsUnit.Point, 238);
+            lbTitle.ForeColor = Color.White;
+            lbTitle.Location = new Point(400, 150);
+            lbTitle.Name = "lbTitle";
+            lbTitle.Size = new Size(96, 54);
+            lbTitle.TabIndex = 4;
+            lbTitle.Text = "Title";
+            // 
+            // lbSongsNumber
+            // 
+            lbSongsNumber.AutoSize = true;
+            lbSongsNumber.Font = new Font("Segoe UI", 20.25F);
+            lbSongsNumber.ForeColor = Color.FromArgb(97, 217, 24);
+            lbSongsNumber.Location = new Point(400, 222);
+            lbSongsNumber.Name = "lbSongsNumber";
+            lbSongsNumber.Size = new Size(182, 37);
+            lbSongsNumber.TabIndex = 5;
+            lbSongsNumber.Text = "Ilość utworów";
+            // 
+            // lbdownloadsNumber
+            // 
+            lbdownloadsNumber.AutoSize = true;
+            lbdownloadsNumber.Font = new Font("Segoe UI", 20.25F);
+            lbdownloadsNumber.ForeColor = Color.FromArgb(97, 217, 24);
+            lbdownloadsNumber.Location = new Point(138, 329);
+            lbdownloadsNumber.Name = "lbdownloadsNumber";
+            lbdownloadsNumber.Size = new Size(163, 37);
+            lbdownloadsNumber.TabIndex = 6;
+            lbdownloadsNumber.Text = "Ilość pobrań";
+            // 
+            // lbYear
+            // 
+            lbYear.AutoSize = true;
+            lbYear.Font = new Font("Segoe UI", 20.25F);
+            lbYear.ForeColor = Color.FromArgb(97, 217, 24);
+            lbYear.Location = new Point(659, 222);
+            lbYear.Name = "lbYear";
+            lbYear.Size = new Size(61, 37);
+            lbYear.TabIndex = 7;
+            lbYear.Text = "Rok";
+            // 
+            // btnDownload
+            // 
+            btnDownload.BackColor = Color.FromArgb(97, 217, 24);
+            btnDownload.FlatAppearance.BorderColor = Color.FromArgb(97, 217, 24);
+            btnDownload.FlatAppearance.BorderSize = 0;
+            btnDownload.FlatStyle = FlatStyle.Flat;
+            btnDownload.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            btnDownload.ForeColor = Color.Black;
+            btnDownload.Location = new Point(400, 321);
+            btnDownload.Name = "btnDownload";
+            btnDownload.Size = new Size(156, 53);
+            btnDownload.TabIndex = 8;
+            btnDownload.Text = "Pobierz";
+            btnDownload.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SeaGreen;
-            ClientSize = new Size(982, 453);
-            Controls.Add(button1);
+            ClientSize = new Size(1389, 479);
+            Controls.Add(btnDownload);
+            Controls.Add(lbYear);
+            Controls.Add(lbdownloadsNumber);
+            Controls.Add(lbSongsNumber);
+            Controls.Add(lbTitle);
+            Controls.Add(lbArtist);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.White;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "MojeDźwięki, Wykonał: XXXXXXXX";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label lbArtist;
+        private Label lbTitle;
+        private Label lbSongsNumber;
+        private Label lbdownloadsNumber;
+        private Label lbYear;
+        private Button btnDownload;
     }
 }
--- desk/ark4/solution/SongsApp/Form1.cs
+++ desk/ark4/solution/SongsApp/Form1.cs
@@ -5,6 +5,7 @@ namespace SongsApp
     public partial class Form1 : Form
     {
         private List<Song> songs;
+        private int currentSongIndex = 0;
         private ISongRepo songRepo = new FileSongRepo();
         public Form1()
         {
@@ -14,6 +15,36 @@ namespace SongsApp
         private void Form1_Load(object sender, EventArgs e)
         {
             songs = songRepo.GetAllSongs().ToList();
+            DisplayCurrentSong();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            currentSongIndex--;
+            if (currentSongIndex < 0)
+            {
+                currentSongIndex = songs.Count - 1;
+            }
+            DisplayCurrentSong();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            currentSongIndex++;
+            if (currentSongIndex >= songs.Count)
+            {
+                currentSongIndex = 0;
+            }
+            DisplayCurrentSong();
+        }
+        private void DisplayCurrentSong()
+        {
+            Song currentSong = songs[currentSongIndex];
+            lbArtist.Text = currentSong.Artist;
+            lbTitle.Text = currentSong.Album;
+            lbSongsNumber.Text = currentSong.SongNumber.ToString();
+            lbYear.Text = currentSong.Year.ToString();
+            lbdownloadsNumber.Text = currentSong.DownloadNumber.ToString();
         }
     }
 }
