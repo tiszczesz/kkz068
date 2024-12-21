257513461ecc387d2c14e5d3b5b31d9ec53f8a3e
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-21 09:26:34 +0100

    jjj

--- desk/cw12/zadanie/Form1.Designer.cs
+++ desk/cw12/zadanie/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(22, 70);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(405, 319);
+            listBox1.TabIndex = 0;
+          
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nowe zadanie:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(161, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(296, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Dodaj zadanie";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 407);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Ilość zadań: 0";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(489, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "al ma kota";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Button button2;
     }
 }
--- desk/cw12/zadanie/Form1.cs
+++ desk/cw12/zadanie/Form1.cs
@@ -2,9 +2,30 @@ namespace zadanie
 {
     public partial class Form1 : Form
     {
+       // private List<string> list = new List<string>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //list.Add(textBox1.Text);
+            // int liczba = Convert.ToInt32(text);
+            //list
+           // listBox1.DataSource = null;
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Text = "";
+            // listBox1.DataSource = list;
+            label2.Text = "iloœæ elementów: "+listBox1.Items.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+            button2.ForeColor = Color.Red;
+        }
+
+      
     }
 }
