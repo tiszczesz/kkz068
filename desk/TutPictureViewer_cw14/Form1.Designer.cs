16e5f7f56987cd8e8fc57f2a98f2b67130978d99
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-21 14:52:48 +0100

    kk

--- desk/TutPictureViewer_cw14/Form1.Designer.cs
+++ desk/TutPictureViewer_cw14/Form1.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            pan1 = new Panel();
+            lbY = new Label();
+            lbX = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            label4 = new Label();
+            label3 = new Label();
+            pan2 = new Panel();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 450);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.FromArgb(255, 255, 192);
+            tabPage1.Controls.Add(pan1);
+            tabPage1.Controls.Add(lbY);
+            tabPage1.Controls.Add(lbX);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 422);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            // 
+            // pan1
+            // 
+            pan1.BorderStyle = BorderStyle.Fixed3D;
+            pan1.Dock = DockStyle.Right;
+            pan1.Location = new Point(133, 3);
+            pan1.Name = "pan1";
+            pan1.Size = new Size(656, 416);
+            pan1.TabIndex = 4;
+            pan1.MouseEnter += pan1_MouseEnter;
+            pan1.MouseLeave += pan1_MouseLeave;
+            pan1.MouseMove += pan1_MouseMove;
+            // 
+            // lbY
+            // 
+            lbY.AutoSize = true;
+            lbY.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
+            lbY.Location = new Point(60, 57);
+            lbY.Name = "lbY";
+            lbY.Size = new Size(29, 30);
+            lbY.TabIndex = 3;
+            lbY.Text = "--";
+            // 
+            // lbX
+            // 
+            lbX.AutoSize = true;
+            lbX.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold);
+            lbX.Location = new Point(60, 21);
+            lbX.Name = "lbX";
+            lbX.Size = new Size(29, 30);
+            lbX.TabIndex = 2;
+            lbX.Text = "--";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F);
+            label2.Location = new Point(24, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F);
+            label1.Location = new Point(24, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 30);
+            label1.TabIndex = 0;
+            label1.Text = "X:";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.FromArgb(192, 255, 192);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(pan2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 422);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "green";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(337, 344);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 2;
+            label4.Text = "green";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(100, 156);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 15);
+            label3.TabIndex = 1;
+            label3.Text = "red";
+            // 
+            // pan2
+            // 
+            pan2.BorderStyle = BorderStyle.FixedSingle;
+            pan2.Location = new Point(217, 52);
+            pan2.Name = "pan2";
+            pan2.Size = new Size(255, 255);
+            pan2.TabIndex = 0;
+            pan2.MouseMove += panel1_MouseMove;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Okienko główne";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Panel pan1;
+        private Label lbY;
+        private Label lbX;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Panel pan2;
+        private Label label4;
     }
 }
--- desk/TutPictureViewer_cw14/Form1.cs
+++ desk/TutPictureViewer_cw14/Form1.cs
@@ -6,5 +6,31 @@ namespace TutPictureViewer_cw14
         {
             InitializeComponent();
         }
+
+        private void pan1_MouseEnter(object sender, EventArgs e)
+        {
+            pan1.BackColor = Color.Red;
+        }
+
+        private void pan1_MouseLeave(object sender, EventArgs e)
+        {
+            pan1.BackColor = Color.FromArgb(255, 255, 192);
+            lbX.Text = "--";
+            lbY.Text = "--";
+        }
+
+        private void pan1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lbX.Text = e.X.ToString();
+            lbY.Text = (pan1.Height - e.Y).ToString();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int red = e.Y;
+            int green = e.X;
+            int blue = (e.X + e.Y) / 2;
+            pan2.BackColor = Color.FromArgb(red, green, blue);
+        }
     }
 }
