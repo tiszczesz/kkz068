5932785ba42e722b020d3c870f91c6e667368a49
tiszczesz <tiszczesz@users.noreply.github.com>
2025-02-16 14:39:37 +0100

    kkk

--- mobile/cw1/app/src/main/java/com/example/cw1/cw4.kt
+++ mobile/cw1/app/src/main/java/com/example/cw1/cw4.kt
@@ -1,32 +1,42 @@
 package com.example.cw1
 
-fun main(){
-   // ex41()
+fun main() {
+    // ex41()
     ex42()
+    print("ala".lowercase() == "Ala".lowercase())
 }
-fun ex41(){
-   val listOfName = listOf("Jan","Anna","Maria","Piotr","Krzysztof","Zbigniew",
-                                "Katarzyna","Wojciech","Małgorzata","Marek")
+
+fun ex41() {
+    val listOfName = listOf(
+        "Jan", "Anna", "Maria", "Piotr", "Krzysztof", "Zbigniew",
+        "Katarzyna", "Wojciech", "Małgorzata", "Marek"
+    )
     println(listOfName.joinToString())
-    for (elem in listOfName){
+    for (elem in listOfName) {
         println(elem)
     }
-   // listOfName.add("Tomasz")
-    println( listOfName.dropLast(1).joinToString())
+    // listOfName.add("Tomasz")
+    println(listOfName.dropLast(1).joinToString())
     println(listOfName.size)
     println(listOfName.joinToString())
 }
-fun ex42(){
-    val mutableListOfName = mutableListOf("Jan","Anna","Maria","Piotr","Krzysztof","Zbigniew",
-        "Katarzyna","Wojciech","Małgorzata","Marek")
+
+fun ex42() {
+    val mutableListOfName = mutableListOf(
+        "Jan", "Anna", "Maria", "Piotr", "Krzysztof", "Zbigniew",
+        "Katarzyna", "Wojciech", "Małgorzata", "Marek"
+    )
     println(mutableListOfName.joinToString())
-    for (elem in mutableListOfName){
+    for (elem in mutableListOfName) {
         println(elem)
     }
     mutableListOfName.add("Tomasz")
     println(mutableListOfName.size)
     println(mutableListOfName.joinToString())
-    mutableListOfName.removeAt(mutableListOfName.size-2)
+    mutableListOfName.removeAt(mutableListOfName.size - 2)
     println(mutableListOfName.joinToString())
-    mutableListOfName.forEach {println(it.uppercase()) }
+    mutableListOfName.forEach { println(it.uppercase()) }
+    val first = mutableListOfName.first { it.length > 5 }
+    println(mutableListOfName.joinToString())
+    println("first: $first")
 }
