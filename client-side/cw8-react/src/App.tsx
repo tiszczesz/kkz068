6e217932eef57e26e0116b9a51cc4b502113f519
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-15 14:24:53 +0100

    oo

--- client-side/cw8-react/src/App.tsx
+++ client-side/cw8-react/src/App.tsx
@@ -1,14 +1,17 @@
-
 import { useState } from 'react'
+import { welcomeTexts } from './data'
 import './App.css'
 
 function App() {
   const [fontSize, setFontSize] = useState(16)
-
+  const [welcomeText, setWelcomeText] = useState(welcomeTexts[0])
   return (
     <>
      <h1 className='header'>Właściwości czcionki</h1>
      <div>Rozmiar: {fontSize}</div>
+     <input style={{width:"100%"}} type='range' 
+     min={0} max={40} value={fontSize}
+     onChange={(e)=>setFontSize(parseInt(e.target.value))} />
     </>
   )
 }
