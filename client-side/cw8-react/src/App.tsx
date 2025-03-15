82a0e2f20ff94e3dfa8dcbff8865b710a28a850c
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-15 14:41:39 +0100

    kk

--- client-side/cw8-react/src/App.tsx
+++ client-side/cw8-react/src/App.tsx
@@ -4,18 +4,32 @@ import './App.css'
 
 function App() {
   const [fontSize, setFontSize] = useState(16)
-  const [welcomeText, setWelcomeText] =
-        useState(welcomeTexts[0]);
+  const [welcomeIndex, setWelcomeIndex] =
+    useState(0);
   console.log("renderowanie komponentu App");
-  
+
 
   return (
     <>
-     <h1 className='header'>Właściwości czcionki</h1>
-     <div>Rozmiar: {fontSize}</div>
-     <input style={{width:"100%"}} type='range' 
-     min={0} max={40} value={fontSize}
-     onChange={(e)=>setFontSize(parseInt(e.target.value))} />
+      <h1 className='header'>Właściwości czcionki</h1>
+      <div>Rozmiar: {fontSize}</div>
+      <input style={{ width: "100%" }} type='range'
+        min={0} max={40} value={fontSize}
+        onChange={(e) => setFontSize(parseInt(e.target.value))} />
+      <div style={{ fontSize: `${fontSize}px` }}>
+        {welcomeTexts[welcomeIndex]}
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <button style={{
+          padding: "10px",
+          backgroundColor: "green",
+          color: "white"
+        }}
+        onClick={()=>{
+          setWelcomeIndex((welcomeIndex+1)%welcomeTexts.length)
+        }}
+        >&gt;&gt;</button>
+      </div>
     </>
   )
 }
