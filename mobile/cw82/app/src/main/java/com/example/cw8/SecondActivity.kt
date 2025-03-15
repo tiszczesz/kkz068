ef2b1c5426f027a2d1945767314697e6a843c819
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-15 13:53:25 +0100

    kkk

--- mobile/cw82/app/src/main/java/com/example/cw8/SecondActivity.kt
+++ mobile/cw82/app/src/main/java/com/example/cw8/SecondActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cw8
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,20 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnMainActivity = findViewById<Button>(R.id.btnMainActivity)
-        btnMainActivity.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+        val btnSecondActivity = findViewById<Button>(R.id.btnSecondActivity)
+        btnSecondActivity.setOnClickListener {
+            finish()
         }
     }
 }
