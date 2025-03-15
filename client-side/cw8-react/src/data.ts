a4ade436251d0dca4bf233eecd0809a2f63c6eb4
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-15 14:13:47 +0100

    kk

---
+++ client-side/cw8-react/src/data.ts
@@ -0,0 +1,12 @@
+export const welcomeTexts = [
+    'Witaj w aplikacji',
+    'Hello in the app',
+    'Hola en la aplicación',
+    'Bonjour dans l\'application',
+    'Hallo in der Anwendung',
+    'Ciao nell\'applicazione',
+    'Olá no aplicativo',
+    'Привет в приложении',
+    '앱에서 안녕하세요',
+    'アプリでこんにちは'
+]
