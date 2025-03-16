e77c75d0ed6b0c8ba6660cad7030fd160d18c652
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-16 12:50:15 +0100

    kk

--- client-side/cw9-todos/src/components/ItemList.tsx
+++ client-side/cw9-todos/src/components/ItemList.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
 
-type Props = {}
+import { User } from '../data/users'
+
+type Props = {
+    user: User
+}
 
 const ItemList = (props: Props) => {
   return (
-    <div>ItemList</div>
+    <div className='user'>
+            <h3>nazwa użytkownika: {props.user.name}</h3>
+            <h5>email: {props.user.email}</h5>
+            <div>telefon: {props.user.phone}</div>
+            <button className='btn btn-danger'>Usuń</button>
+    </div>
   )
 }
 
