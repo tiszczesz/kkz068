49239e5b975485d48c2de9e03ea9ac886fb31347
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-16 09:25:43 +0100

    kkk

--- mobile/cw10_list/app/src/main/java/com/example/cw10_list/MainActivity.kt
+++ mobile/cw10_list/app/src/main/java/com/example/cw10_list/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.cw10_list
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +24,31 @@ class MainActivity : AppCompatActivity() {
         val etAdd = findViewById<EditText>(R.id.etAdd)
         val btnAdd = findViewById<Button>(R.id.btnAdd)
         val lvContacts = findViewById<ListView>(R.id.lvContacts)
+        val adapterList = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1, contacts
+        )
+        // set the adapter to the list view datagridview.DataSource = contacts
+        lvContacts.adapter = adapterList;
+        btnAdd.setOnClickListener {
+            val contact = etAdd.text.toString()
+            if (contact.isNotEmpty()) {
+                contacts.add(contact)
+                adapterList.notifyDataSetChanged()
+                etAdd.text.clear()
+            }
+        }
+
+        lvContacts.setOnItemClickListener { parent, view, position, id ->
+            val contact = contacts[position]
+            Toast.makeText(this, contact, Toast.LENGTH_SHORT).show()
+        }
+        lvContacts.setOnItemLongClickListener { parent, view, position, id ->
+            contacts.removeAt(position)
+            adapterList.notifyDataSetChanged()
+            true
+        }
+       
+
     }
 }
