c90af519c0b88dc000d7def575db6a0ea7042f6b
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-30 13:53:36 +0200

    jk

---
+++ client-side/cw10-2025/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
