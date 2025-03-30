45dc962e9d71ef2f80b651397cd920d9b9de3eae
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-30 10:27:26 +0200

    mm

--- mobile/cw11_klasy/app/src/main/java/com/example/cw11_klasy/cw11_main2.kt
+++ mobile/cw11_klasy/app/src/main/java/com/example/cw11_klasy/cw11_main2.kt
@@ -1,5 +1,9 @@
 package com.example.cw11_klasy
 
+import java.time.Year
+import java.util.Calendar
+import java.util.Date
+
 fun main() {
     val s1 = Samochod("Audi", "A4", 2020)
     val s2 = Samochod2("BMW", "X5", 2021)
@@ -7,9 +11,17 @@ fun main() {
     //uzycie metody toString
     println(s1)
     println(s2)
+    val b1 = Book("  Wojna i pokój  ", "L. Tołstoj", 1869)
+    println(b1)
+    b1.Title = "ala ma kota"
+    b1.Year = 2029
+    println(b1)
+    val b2 = Book()
+    println(b2)
+    b2.Inna()
 }
 
-//klasa w kotlinie z domyslnym konstruktorem wersja klasyczna
+//klasa w kotlinie z  konstruktorem wersja klasyczna
 class Samochod {
     private var marka: String = ""
     protected var model: String = ""
@@ -26,10 +38,49 @@ class Samochod {
     }
 }
 
-//klasa w kotlinie z domyslnym konstruktorem wersja uproszczona
+//klasa w kotlinie z  konstruktorem wersja uproszczona
 class Samochod2(private var marka: String, protected var model: String, var rocznik: Int) {
     //metoda toString
     override fun toString(): String {
         return "Samochod(marka='$marka', model='$model', rocznik=$rocznik)"
+    }
+}
+
+
+class Book(private var title: String, private var author: String, private var year: Int) {
+    //metoda wywołana podczas tworzenia obiektu
+    init {
+        println("Tworzenie obiektu Book")
+        this.title = title.trim()
+        this.author = author.uppercase()
+    }
+    //konstruktor bezparametrowy
+    constructor() : this("noname", "noname", 2000) {
+        println("Tworzenie obiektu Book bezparametrowego")
+    }
+    public var Title
+        get() = ">>>>$title<<<<"
+        set(value) {
+            title = value.trim()
+        }
+    var Year
+        get() = year
+        set(value) {
+            val calendar = Calendar.getInstance()
+            val currentYear = calendar.get(Calendar.YEAR)
+            if (value >= currentYear) {
+                println("Nie można ustawić przyszłego roku")
+                year = currentYear
+            } else {
+                year = value
+            }
+        }
+    //metoda toString
+    override fun toString(): String {
+        return "Book(title='$Title', author='$author', year=$year)"
+    }
+
+    fun Inna(){
+        println("Inna metoda: ${toString()}")
     }
 }
