f88552a45571fcd26175f2074aa9542614bc4401
tiszczesz <tiszczesz@users.noreply.github.com>
2025-04-13 09:57:17 +0200

    kk

--- client-side/cw8-2025/cw8-ark2025/src/App.tsx
+++ client-side/cw8-2025/cw8-ark2025/src/App.tsx
@@ -1,13 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-
+  const [animalChecked, setAnimalChecked] = useState(true);
+  const [flowersChecked, setFlowersChecked] = useState(true);
+  const [carsChecked, setCarsChecked] = useState(true);
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Kategorie zdjęć</h1>
+      <div style={{display:"flex",gap:"10px"}}>
+      <div className="form-check form-switch">
+        <input onClick={()=>setFlowersChecked(!flowersChecked)} className="form-check-input"
+          type="checkbox" id="flowers"  checked={flowersChecked}/>
+        <label className="form-check-label"
+          htmlFor='flowers'>Kwiaty</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input"
+          type="checkbox" id="animals" checked={animalChecked}/>
+        <label className="form-check-label"
+          htmlFor='animals'>Zwierzęta</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input"
+          type="checkbox" id="cars"  checked={carsChecked}/>
+        <label className="form-check-label"
+          htmlFor='cars'>Samochody</label>
+      </div>
 
+      </div>
+      
     </>
   )
 }
