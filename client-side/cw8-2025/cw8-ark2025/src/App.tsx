fd595908770fa8f9076321eb80017dccd640e7c4
tiszczesz <tiszczesz@users.noreply.github.com>
2025-04-13 10:02:15 +0200

    kk

--- client-side/cw8-2025/cw8-ark2025/src/App.tsx
+++ client-side/cw8-2025/cw8-ark2025/src/App.tsx
@@ -10,29 +10,29 @@ function App() {
   return (
     <>
       <h1>Kategorie zdjęć</h1>
-      <div style={{display:"flex",gap:"10px"}}>
-      <div className="form-check form-switch">
-        <input onClick={()=>setFlowersChecked(!flowersChecked)} className="form-check-input"
-          type="checkbox" id="flowers"  checked={flowersChecked}/>
-        <label className="form-check-label"
-          htmlFor='flowers'>Kwiaty</label>
-      </div>
-      <div className="form-check form-switch">
-        <input onClick={()=>setAnimalChecked(!animalChecked)} className="form-check-input"
-          type="checkbox" id="animals" checked={animalChecked}/>
-        <label className="form-check-label"
-          htmlFor='animals'>Zwierzęta</label>
-      </div>
-      <div className="form-check form-switch">
-        <input className="form-check-input"
-          onClick={()=>setCarsChecked(!carsChecked)}
-          type="checkbox" id="cars"  checked={carsChecked}/>
-        <label className="form-check-label"
-          htmlFor='cars'>Samochody</label>
-      </div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <div className="form-check form-switch">
+          <input onClick={() => setFlowersChecked(!flowersChecked)} className="form-check-input"
+            type="checkbox" id="flowers" checked={flowersChecked} />
+          <label className="form-check-label"
+            htmlFor='flowers'>Kwiaty</label>
+        </div>
+        <div className="form-check form-switch">
+          <input onClick={() => setAnimalChecked(!animalChecked)} className="form-check-input"
+            type="checkbox" id="animals" checked={animalChecked} />
+          <label className="form-check-label"
+            htmlFor='animals'>Zwierzęta</label>
+        </div>
+        <div className="form-check form-switch">
+          <input className="form-check-input"
+            onClick={() => setCarsChecked(!carsChecked)}
+            type="checkbox" id="cars" checked={carsChecked} />
+          <label className="form-check-label"
+            htmlFor='cars'>Samochody</label>
+        </div>
 
       </div>
-      
+     
     </>
   )
 }
